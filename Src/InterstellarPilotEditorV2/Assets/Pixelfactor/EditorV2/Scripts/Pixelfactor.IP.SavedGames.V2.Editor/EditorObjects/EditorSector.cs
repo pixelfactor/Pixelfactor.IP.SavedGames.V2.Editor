b44a299d7dfb37a5cd4fbec0d667e6c522a939f1
George Daters <admin@pixelfactor.com>
2023-01-08 17:46:02 +0000

    Sector documentation

--- Src/InterstellarPilotEditorV2/Assets/Pixelfactor/EditorV2/Scripts/Pixelfactor.IP.SavedGames.V2.Editor/EditorObjects/EditorSector.cs
+++ Src/InterstellarPilotEditorV2/Assets/Pixelfactor/EditorV2/Scripts/Pixelfactor.IP.SavedGames.V2.Editor/EditorObjects/EditorSector.cs
@@ -8,43 +8,62 @@ namespace Pixelfactor.IP.SavedGames.V2.Editor.EditorObjects
     /// </summary>
     public class EditorSector : MonoBehaviour
     {
-        public int Id;
+        [Tooltip("A unique sector Id. If the value is less than zero (the default) a unique id will be automatically assigned when exporting")]
+        public int Id = -1;
 
+        [Tooltip("The name of the sector as shown in the engine")]
         public string Name;
 
+        /// <summary>
+        /// Reserved for future use - A description of the sector as shown to the player.
+        /// </summary>
+        [HideInInspector]
+        [Tooltip("Reserved for future use - A description of the sector as shown to the player.")]
         public string Description;
 
         /// <summary>
         /// Determines the distance of the wormholes from the sector origin. Should be ~1.0
         /// </summary>
+        [Tooltip("Determines the size of the playing area of the sector. This value is used extensively by the engine when determining where to place units. Use 0 to make the smallest sector possible. This value is respected when using the 'connect tool' to create wormholes.")]
+        [Range(0f, 1f)]
         public float WormholeDistanceMultiplier = 1.0f;
 
         /// <summary>
         /// Something to do with the appearance of asteroid clusters
         /// </summary>
+        [Tooltip("A seed used to determine the appearance of the sector e.g. procedural sky. When unset, a seed will be assigned randomly at runtime. Set a value to have a consistent appearance on each play.")]
         public int RandomSeed = -1;
 
         /// <summary>
         /// Skybox rotation
         /// </summary>
+        [Tooltip("An optional rotation (fudge) applied to the sector sky. Use this to tweak the sky background.")]
         public Vector3 BackgroundRotation;
 
+        /// <summary>
+        /// Reserved for future use - the rotation of the sectors directional light.
+        /// </summary>
+        [Tooltip("Reserved for future use - the rotation of the sectors directional light.")]
+        [HideInInspector]
         public Vector3 DirectionLightRotation;
 
         /// <summary>
         /// Alpha ignored
         /// </summary>
+        [Tooltip("The colour of the sectors ambient light. The alpha component is not used")]
         public Color AmbientLightColor = new Color(0.3f, 0.3f, 0.3f, 1.0f);
 
         /// <summary>
         /// Alpha ignored
         /// </summary>
+        [Tooltip("The colour of the sectors directional light. The alpha component is not used")]
         public Color DirectionLightColor = new Color(0.8f, 0.8f, 0.8f, 1.0f);
 
         /// <summary>
-        /// Changes the initial light direction at the start of the game
+        /// Changes the direction light direction at the start of the game
         /// </summary>
         [Range(0.0f, 1.0f)]
+        [Tooltip("The direction of the sector's directional light at the start of the game")]
         public float LightDirectionFudge = 0.0f;
 
         void OnDrawGizmosSelected()
